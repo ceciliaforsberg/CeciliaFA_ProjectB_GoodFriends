201f54bc99b82b0ad1ff780e6737c44d95cb438b
Cecilia Forsberg Andersson <cecilia.forsbergand@learnet.se>
2026-01-02 16:05:17 +0100

    Page update

--- AppRazor/Pages/Friends/Overview.cshtml.cs
+++ AppRazor/Pages/Friends/Overview.cshtml.cs
@@ -19,26 +19,67 @@ namespace AppRazor.Pages
 
         [BindProperty]
         public bool UseSeeds { get; set; } = true;
+        [BindProperty]
         public int NrOfFriends {get; set;}
+        [BindProperty]
+        public string? ExpandedCountry { get; set; }
+        [BindProperty]
+        public string? PreviouslyExpandedCountry { get; set; }
+
         public List<string> AvalibleCountries { get; set; } = new();
+        public List<string> ExpandedCities { get; set; } = new();
+        public Dictionary<string,(int FriendsCount, int PetsCount)> CityData { get; set;} = new();
         public Dictionary<string,(int FriendsCount, int PetsCount)> CountryData { get; set; } = new();
+
         public async Task<IActionResult> OnGet(OverviewMode mode)
         {
             Mode = mode;
+
+            await LoadCountriesAsync();
+
+            return Page();
+        }
+        public async Task<IActionResult> OnPostExpandCountryAsync()
+        {
+            if (string.IsNullOrWhiteSpace(ExpandedCountry))
+            return Page();
+
+            if (ExpandedCountry == PreviouslyExpandedCountry) ExpandedCountry = null;
+
+            await LoadCountriesAsync();
+
+            await LoadCitiesForCountry(NrOfFriends);
+            return Page();
+
+        }
+        private async Task LoadCountriesAsync()
+        {
             AvalibleCountries = await _addressService.ReadAllCountriesAsync(UseSeeds);
 
             var totalResp = await _friendService.ReadFriendsAsync(UseSeeds, false, null, 0, 1);
-            int totalItems = totalResp.DbItemsCount;
+            NrOfFriends = totalResp.DbItemsCount;
 
             foreach (var country in AvalibleCountries)
             {
-                var resp = await _friendService.ReadFriendsAsync(UseSeeds, false, country, 0, totalItems);
+                var resp = await _friendService.ReadFriendsAsync(UseSeeds, false, country, 0, NrOfFriends);
                 
                 int friendsCount = resp.PageItems.Count;
                 int petsCount = resp.PageItems.Sum(f => f.Pets != null ? f.Pets.Count : 0);
                 CountryData.Add(country, (friendsCount, petsCount));
             }
-            return Page();
+        }
+        private async Task LoadCitiesForCountry(int totalItems)
+        {
+
+            ExpandedCities = await _addressService.ReadAllCitiesAsync(UseSeeds,ExpandedCountry);
+            foreach(var city in ExpandedCities)
+            {
+                var resp = await _friendService.ReadFriendsAsync(UseSeeds, false, city, 0, totalItems);
+
+                int friendsCount = resp.PageItems.Count;
+                int petsCount = resp.PageItems.Sum(f => f.Pets != null ? f.Pets.Count : 0);
+                CityData.Add(city, (friendsCount, petsCount));
+            }
         }
         public OverviewModel(IFriendsService friendService, IAddressesService addressService ,ILogger<SeedModel> logger)
         {
