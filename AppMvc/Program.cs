945365bc44f0560e02dbea6c1aeec9432fbc1a4a
Cecilia Forsberg Andersson <cecilia.forsbergand@learnet.se>
2026-01-07 18:35:08 +0100

    Setup

--- AppMvc/Program.cs
+++ AppMvc/Program.cs
@@ -1,29 +1,57 @@
-var builder = WebApplication.CreateBuilder(args);
+using Services;
+using Configuration.Extensions;
+using DbContext.Extensions;
+using DbRepos;
+using Encryption.Extensions;
+using Services.Interfaces;
+using Encryption;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+builder.Configuration.AddSecrets(builder.Environment);
+
+builder.Services.AddEncryptions(builder.Configuration);
+builder.Services.AddDatabaseConnections(builder.Configuration);
+builder.Services.AddJwtToken(builder.Configuration);
+builder.Services.AddUserBasedDbContext();
+
+builder.Services.AddVersionInfo();
+builder.Services.AddEnvironmentInfo();
+
+builder.Services.AddScoped<AdminDbRepos>();
+builder.Services.AddScoped<AddressesDbRepos>();
+builder.Services.AddScoped<FriendsDbRepos>();
+builder.Services.AddScoped<LoginDbRepos>();
+builder.Services.AddScoped<PetsDbRepos>();
+builder.Services.AddScoped<QuotesDbRepos>();
+
+builder.Services.AddScoped<JwtEncryptions>();
+
+builder.Services.AddScoped<IAdminService, AdminServiceDb>();
+builder.Services.AddScoped<IFriendsService, FriendsServiceDb>();
+builder.Services.AddScoped<IAddressesService, AddressesServiceDb>();
+builder.Services.AddScoped<IPetsService, PetsServiceDb>();
+builder.Services.AddScoped<IQuotesService, QuotesServiceDb>();
+builder.Services.AddScoped<ILoginService, LoginService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security
+    //https://learn.microsoft.com/en-us/aspnet/core/security/enforcing-ssl
     app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
